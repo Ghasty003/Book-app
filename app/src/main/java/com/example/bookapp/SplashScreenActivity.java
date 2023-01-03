c81b5bbade66b16b9926f07ee919aa73669fdf99
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-03 15:23:44 -0400

    Completed Login activity

--- app/src/main/java/com/example/bookapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/bookapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+        }, 1000);
     }
 }
