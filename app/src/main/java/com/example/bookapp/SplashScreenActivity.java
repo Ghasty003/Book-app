0322654273a916b48d27a5eed7e8334fffcf45d2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-03 19:01:36 -0400

    updated

--- app/src/main/java/com/example/bookapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/bookapp/SplashScreenActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
@@ -13,9 +16,23 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-            finish();
-        }, 1000);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null && user.getEmail().equals("admin@dev.com")) {
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashScreenActivity.this, AdminControlActivity.class));
+                finish();
+            }, 1000);
+        } else if (user != null) {
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }, 1000);
+        } else {
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }, 1000);
+        }
     }
 }
