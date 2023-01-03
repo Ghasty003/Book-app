0c22f9ef5ac1ae106160af0fcce7bb65dc824573
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-03 18:19:51 -0400

    Initialized admin activity

---
+++ app/src/main/java/com/example/bookapp/AdminControlActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_control);
+    }
+}
