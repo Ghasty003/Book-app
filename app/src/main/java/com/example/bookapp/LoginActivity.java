7397aeebf64da961c42e70bb5cfce03378f0a747
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-03 15:47:45 -0400

    updated

--- app/src/main/java/com/example/bookapp/LoginActivity.java
+++ app/src/main/java/com/example/bookapp/LoginActivity.java
@@ -2,13 +2,23 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(view -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
     }
 }
