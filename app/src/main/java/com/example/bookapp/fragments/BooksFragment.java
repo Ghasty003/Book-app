1d160cd805cc66e3461fad1127afd939ffd98649
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 17:24:10 -0400

    updated

--- app/src/main/java/com/example/bookapp/fragments/BooksFragment.java
+++ app/src/main/java/com/example/bookapp/fragments/BooksFragment.java
@@ -23,8 +23,7 @@ public class BooksFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_books, container, false);
     }
