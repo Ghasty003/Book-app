090fb61ac6601dc97b40f01eaa71cfe8d83bda95
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 11:56:01 -0400

    make drawer menu close

--- app/src/main/java/com/example/bookapp/AdminControlActivity.java
+++ app/src/main/java/com/example/bookapp/AdminControlActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +15,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AdminControlActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     FirebaseAuth firebaseAuth;
 
@@ -21,6 +24,7 @@ public class AdminControlActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_control);
 
+        drawerLayout = findViewById(R.id.drawer_menu);
         navigationView = findViewById(R.id.navigationView);
         TextView textView = findViewById(R.id.test);
 
@@ -38,16 +42,20 @@ public class AdminControlActivity extends AppCompatActivity {
                 case R.id.logout:
                     firebaseAuth.signOut();
                     startActivity(new Intent(this, LoginActivity.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     finish();
                     break;
                 case R.id.display_users:
                     startActivity(new Intent(this, DisplayUsersActivity.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     break;
                 case R.id.add:
                     startActivity(new Intent(this, AddNewBookActivity.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     break;
                 case R.id.display:
                     startActivity(new Intent(this, DisplayBooksActivity.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     break;
             }
 
