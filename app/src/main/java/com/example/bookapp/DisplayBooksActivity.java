c8060f17f95e2dc35cf89bdab2f0244f6c0571f4
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 11:13:06 -0400

    setup recyclerview for books

--- app/src/main/java/com/example/bookapp/DisplayBooksActivity.java
+++ app/src/main/java/com/example/bookapp/DisplayBooksActivity.java
@@ -1,14 +1,53 @@
 package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.Query;
+
 public class DisplayBooksActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    BooksAdapter booksAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_books);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        displayBooks();
+    }
+
+    private void displayBooks() {
+        Query query = Utility.getCollectionReferenceForBooks().orderBy("bookName", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<BookUpload> options = new FirestoreRecyclerOptions.Builder<BookUpload>().setQuery(query, BookUpload.class).build();
+
+        booksAdapter = new BooksAdapter(options, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(booksAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        booksAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        booksAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        booksAdapter.notifyDataSetChanged();
     }
 }
