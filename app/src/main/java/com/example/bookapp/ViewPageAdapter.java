c6c132666f1ace317a8cd10e619fac073ac7858a
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 18:10:03 -0400

    completed view pager adapter

--- app/src/main/java/com/example/bookapp/ViewPageAdapter.java
+++ app/src/main/java/com/example/bookapp/ViewPageAdapter.java
@@ -1,4 +1,35 @@
 package com.example.bookapp;
 
-public class ViewPageAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.bookapp.fragments.BooksFragment;
+import com.example.bookapp.fragments.LibraryFragment;
+import com.example.bookapp.fragments.SettingsFragment;
+
+public class ViewPageAdapter extends FragmentStateAdapter {
+
+    public ViewPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+       switch (position) {
+           case 1:
+               return new BooksFragment();
+           case 2:
+               return new SettingsFragment();
+           default:
+               return new LibraryFragment();
+       }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
 }
