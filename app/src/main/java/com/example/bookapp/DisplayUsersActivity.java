b55a75805079a85ba745fdd4cdcb71b0308d939c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 13:36:39 -0400

    added back button functionality

--- app/src/main/java/com/example/bookapp/DisplayUsersActivity.java
+++ app/src/main/java/com/example/bookapp/DisplayUsersActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.Query;
@@ -20,6 +24,13 @@ public class DisplayUsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_users);
 
         recyclerView = findViewById(R.id.users_recyclerView);
+
+        View header = findViewById(R.id.header);
+        ImageButton img = header.findViewById(R.id.back_btn);
+
+        img.setOnClickListener(view -> {
+            startActivity(new Intent(this, AdminControlActivity.class));
+        });
 
         setUpRecyclerView();
     }
