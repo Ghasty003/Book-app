7c88d4cc215b8b23c944aa8791b204f955f3fb5d
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-03 21:16:35 -0400

    Added Utility class

--- app/src/main/java/com/example/bookapp/Users.java
+++ app/src/main/java/com/example/bookapp/Users.java
@@ -1,4 +1,32 @@
 package com.example.bookapp;
 
 public class Users {
+    String email, password, username;
+
+    public Users() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
