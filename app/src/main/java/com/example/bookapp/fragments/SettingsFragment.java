0a5702e9e5633eb240c6ccc1b0ed26090fb6a248
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-04 22:01:15 -0400

    added logout functionality

--- app/src/main/java/com/example/bookapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/bookapp/fragments/SettingsFragment.java
@@ -1,20 +1,43 @@
 package com.example.bookapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.bookapp.LoginActivity;
 import com.example.bookapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment {
+
+    private TextView logout;
 
 
     public SettingsFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        logout = view.findViewById(R.id.logout);
+
+
+
+        logout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getContext(), LoginActivity.class));
+            getActivity().finish();
+        });
     }
 
     @Override
