5f49ee5003d3c7b27a55db900f556c99ed5140e1
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-05 10:58:24 -0400

    added user details change activity

---
+++ app/src/main/java/com/example/bookapp/UserDetailsChangeActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDetailsChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details_change);
+    }
+}
