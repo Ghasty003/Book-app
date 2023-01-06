54fb31dc454fd1a510c18947d82b0f94d385aa44
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-05 21:35:22 -0400

    added view holder

--- app/src/main/java/com/example/bookapp/DeleteBooksAdapter.java
+++ app/src/main/java/com/example/bookapp/DeleteBooksAdapter.java
@@ -1,4 +1,28 @@
 package com.example.bookapp;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class DeleteBooksAdapter {
+
+    class DeleteBookViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        ImageButton imageButton;
+        TextView authorName, bookName;
+
+        public DeleteBookViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.delete_book_image_view);
+            imageButton = itemView.findViewById(R.id.delete_item_icon);
+            authorName = itemView.findViewById(R.id.delete_author_text_view);
+            bookName = itemView.findViewById(R.id.delete_book_text_view);
+        }
+    }
 }
