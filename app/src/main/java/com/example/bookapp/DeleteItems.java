21a64a7046d876da2c4ffd06c67a3b2851625d99
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-05 21:47:21 -0400

    completed class

--- app/src/main/java/com/example/bookapp/DeleteItems.java
+++ app/src/main/java/com/example/bookapp/DeleteItems.java
@@ -1,4 +1,26 @@
 package com.example.bookapp;
 
+import android.widget.ImageView;
+
 public class DeleteItems {
+    String authorName, bookName;
+    ImageView bookImage;
+
+    public DeleteItems(String authorName, String bookName, ImageView bookImage) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.bookImage = bookImage;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public ImageView getBookImage() {
+        return bookImage;
+    }
 }
