3ede4318c6a10e233099dabcb4a0a06983385373
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 10:40:46 -0400

    updated items

--- app/src/main/java/com/example/bookapp/DeleteItems.java
+++ app/src/main/java/com/example/bookapp/DeleteItems.java
@@ -4,10 +4,8 @@ package com.example.bookapp;
 public class DeleteItems {
     String authorName, bookName, bookImage;
 
-    public DeleteItems(String authorName, String bookName, String bookImage) {
-        this.authorName = authorName;
-        this.bookName = bookName;
-        this.bookImage = bookImage;
+    public DeleteItems() {
+
     }
 
     public String getAuthorName() {
