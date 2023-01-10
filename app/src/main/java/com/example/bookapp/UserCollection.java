f6021c6adbad53d824413f22d5b268e0c6ef34c8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-10 14:37:25 -0400

    Completed user collection class

--- app/src/main/java/com/example/bookapp/UserCollection.java
+++ app/src/main/java/com/example/bookapp/UserCollection.java
@@ -1,4 +1,32 @@
 package com.example.bookapp;
 
 public class UserCollection {
+    String authorName, bookName, imageUri;
+
+    public UserCollection() {
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
 }
