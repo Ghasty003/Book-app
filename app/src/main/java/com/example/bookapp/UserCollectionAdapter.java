99e9e15cd0d2d3720f18c5e5a7cad6adc443908a
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-10 15:49:36 -0400

    Added view holder

--- app/src/main/java/com/example/bookapp/UserCollectionAdapter.java
+++ app/src/main/java/com/example/bookapp/UserCollectionAdapter.java
@@ -1,4 +1,21 @@
 package com.example.bookapp;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class UserCollectionAdapter {
+
+
+    class UserCollectionViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        public UserCollectionViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+        }
+    }
 }
