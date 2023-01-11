1246bc6ec42a03d1cdd05ee5b5acd969bdf8bd20
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-11 11:24:36 -0400

    added delete user item

--- app/src/main/java/com/example/bookapp/UserCollectionAdapter.java
+++ app/src/main/java/com/example/bookapp/UserCollectionAdapter.java
@@ -1,5 +1,6 @@
 package com.example.bookapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +32,28 @@ public class UserCollectionAdapter extends FirestoreRecyclerAdapter<UserCollecti
         holder.authorName.setText(collection.getAuthorName());
         holder.bookName.setText(collection.getBookName());
         Picasso.with(context).load(collection.getImageUri()).into(holder.imageView);
+
+        String docId = this.getSnapshots().getSnapshot(position).getId();
+
+        holder.imageButton.setOnClickListener(view -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+            alertDialog.setTitle("Delete item from collection");
+            alertDialog.setMessage("Are you sure you want to delete? Action is irreversible");
+            alertDialog.setButton(alertDialog.BUTTON_POSITIVE, "Yes", ((dialogInterface, i) -> {
+                Utility.getCollectionReferenceForUsersBook().document(docId).delete().addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(context, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    Toast.makeText(context, "Item removed from collection successfully.", Toast.LENGTH_SHORT).show();
+                });
+            }));
+            alertDialog.setButton(alertDialog.BUTTON_NEGATIVE, "Cancel", ((dialogInterface, i) -> {
+                dialogInterface.dismiss();
+            }));
+            alertDialog.show();
+        });
     }
 
     @NonNull
@@ -39,7 +63,7 @@ public class UserCollectionAdapter extends FirestoreRecyclerAdapter<UserCollecti
         return new UserCollectionViewHolder(view);
     }
 
-    class UserCollectionViewHolder extends RecyclerView.ViewHolder {
+    static class UserCollectionViewHolder extends RecyclerView.ViewHolder {
 
         ImageView imageView;
         ImageButton imageButton;
